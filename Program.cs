efc50bdf651c8533236a6ef76622155a4fdd1121
çağrı dönmez <ertgrldnmz@outlook.com>
2022-11-23 16:59:32 +0300

    readme and gitignore added in project,

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("lütfen bir sayı giriniz");
+        //birden başlayarak konsoldan girilen sayıların ortalaması(While)
+        int x=Convert.ToInt32(Console.ReadLine());
+        int sayac=1;
+        int toplam=0;
+       while (sayac<=x)
+       {
+        toplam+=sayac;
+
+        sayac++;
+
+
+       }
+       Console.WriteLine(toplam/x);
+
+       char character='a';
+       while (character<'z')
+       {
+        Console.WriteLine(character);
+        character++;
+        
+       }
+
+       //Foreach
+       string [] araba={"bmv","ford","toyota","nissan"};
+       foreach (var item in araba)
+       {
+        Console.WriteLine(item);
+        
+       }
+    }
+
+}
